6f56fe52c6aaffae94cb732f94bf5f8077a7f88f
snap-balance <2022141@student.cct.ie>
2022-11-01 21:58:41 +0000

    changes in people.page.ts

--- mobdev-ionic-intro/src/app/pages/people/people.page.ts
+++ mobdev-ionic-intro/src/app/pages/people/people.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-people',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.page.scss'],
 })
 export class PeoplePage implements OnInit {
+  people: Observable<any>;
+  constructor(private router: Router, private http: HttpClient) { }
 
-  constructor() { }
 
   ngOnInit() {
-  }
+    this.people = this.http.get('https://swapi.dev/api/people');
+    this.people.subscribe(data => {
+      console.log('my data: ', data);
+      
+});
 
+
+  }
+  
 }
